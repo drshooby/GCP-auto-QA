51dbfadc57f99757c56134f8d8f8dba95787c792
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-07-31 20:09:02 -0700

    update: cloud func receives json

--- functions/status/main.py
+++ functions/status/main.py
@@ -1,10 +1,6 @@
 def status_get(request):
-    status = request.args.get("status")
-    if status:
-        return f"Execution status: {status}", 200
-
     request_json = request.get_json(silent=True)
-    if request_json and "status" in request_json:
-        return f"Execution status: {request_json["status"]}", 200
+    if request_json and "results" in request_json:
+        return "Test report received", 200
 
-    return "Missing 'status' in request", 400
+    return "Missing 'results' in request", 400
