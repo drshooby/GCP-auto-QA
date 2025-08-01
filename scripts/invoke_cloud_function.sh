eeb734a602046d7f00f622fabb4cba10271e7fcb
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-07-31 20:09:40 -0700

    update: new runner script

--- scripts/invoke_cloud_function.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-STATUS=$1
-
-TOKEN=$(gcloud auth print-identity-token)
-curl -H "Authorization: Bearer $TOKEN" "$(cd ../terraform && terraform output -raw cloud_function_invoke_url)?status=${STATUS}"
