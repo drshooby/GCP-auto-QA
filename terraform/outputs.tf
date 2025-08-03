6fc9607ce5e398db982a5970b7a0980998a44723
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-08-02 21:44:48 -0700

    update: comment out, not needed currently

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "cloud_function_invoke_url" {
-  value = google_cloudfunctions2_function.function.service_config[0].uri
-}
+# output "cloud_function_invoke_url" {
+#   value = google_cloudfunctions2_function.function.service_config[0].uri
+# }
